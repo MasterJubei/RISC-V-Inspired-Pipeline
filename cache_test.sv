7c200021b775f1fbc8cab78cb426933c3a49d85e
jigglywiggly <jigglywiggly@3dslice.net>
2021-05-22 18:10:42 -0700

    Adding the ability for the cache to load cache lines from memory

--- cache_test.sv
+++ cache_test.sv
@@ -21,6 +21,7 @@ module tb();
     wire mem_load_req_f_cache;
     wire mem_store_req_f_cache;
     wire mem_load_req_rdy;
+    wire mem_load_toggle;       /* This toggles back and forward when loading a cache block */
     wire mem_store_completed;   /*Memory confirming it has stored cache block */
     wire mem_store_ack;         /*Cache confirming it has ack the cache block write */
     wire [31:0] mem_data;       /*Cache block from memory*/
@@ -48,7 +49,7 @@ module tb();
         .mem_load_req_rdy(mem_load_req_rdy),
         .mem_store_processed(mem_store_completed),
         .mem_store_ack(mem_store_ack),
-
+        .mem_load_toggle(mem_load_toggle),
         .address_to_mem(address_f_cache)
         );
 
@@ -60,7 +61,9 @@ module tb();
         .wren(mem_store_req_f_cache),
         .load_completed(mem_load_req_rdy),
         .store_completed(mem_store_completed),
-        .store_ack (mem_store_ack)
+        .store_ack (mem_store_ack),
+        .mem_load_req (mem_load_req_f_cache),
+        .load_toggle (mem_load_toggle)
     );
 
     
@@ -83,7 +86,7 @@ module cache_controller(
 
 endmodule
 
-module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
+module cache #(parameter CACHE_NUM_ROW = 8 /*Number of rows in the cache */
     //parameter WIDTH_AD   = 8
     )
     (
@@ -95,21 +98,22 @@ module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
     input INSTR_TYPE instr_type,   // 0 = read, 1 = write
 
     output reg [31:0] data_to_mem,  /*The data(cache block) that should be saved from the cache to memory */
+    input [31:0] data_f_mem,        /*The data from memory that will go in a cache block*/
     output reg mem_load_req,        /*If high, need to load data from memory into cache*/
     output reg mem_store_req,       /*If high, need to save cache block to memory */
     output reg mem_store_ack,       /*If high, then the memory has succesfully stored */
     input mem_store_processed,      /*If high, memory has stored 4 bytse from the cache block.*/
     input mem_load_req_rdy,         /*If high, the data loaded from memory is ready */
-
+    input mem_load_toggle,          /* This is not used, this would be if we needed to 'burst' the ram, aka if we needed multiple clock cycles to fill the cache block*/
     output reg [15:0] address_to_mem /*Address to memory, used when requesting data to load/store*/
+    
 ); 
-    reg [7:0] vld;
+    reg vld [0:7];                  /* 1 bit for each row  */
     reg [31:0] cache_data [0:7];    /* If you load address 0x02, get the data for 0x00 as well in the cache. If you grab 0x00, also get 0x02 */
-    reg [10:0] tag_number [0:7]; 
+    reg [10:0] tag_number [0:7];    /* We have a tag number for each cache line */
     reg [15:0] block_address;       /* We will divide address by cache size to get block address */ 
     reg [15:0] block_num;           /* We will modulo the block addres by cache size to get the column */
     reg [7:0] to_evict;             /* If any of the bits are set, the cache block should be written to memory and freed from the cache */
-
 
     parameter BL_NUM_BYTES = 4; /*Number of bytes in a block, aka number of columns */
     reg read_success = 0;   
@@ -144,8 +148,8 @@ module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
                 $finish;
             end
 
-            block_address = address_in / BL_NUM_BYTES;
-            block_num = block_address % CACHE_NUM_COL;
+            block_address = address_in / BL_NUM_BYTES; /* divide by number of columns to find the row # */
+            block_num = block_address % CACHE_NUM_ROW; /* modulo to get the physical row number */ 
             $display("block_address is %d", block_address );
             $display("block_num is %d", block_num);
 
@@ -163,12 +167,14 @@ module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
                     end
                 end
                 if(read_success == 0) begin
-                    $display("Shouldn't get here just yet \n");
+                    /* We want to read the whole cache line from memory, since the memory has a 32 bit output, this is easy */
                     mem_load_req = 1;
                     wait (mem_load_req_rdy == 1) begin
-                        mem_load_req = 0;
-                    end
-
+                        cache_data[block_num] = data_f_mem;
+                    end 
+                    vld[block_num] = 1;
+                    tag_number[block_num] = address_in[15:5];
+                    
                 end
             end
 
@@ -178,7 +184,13 @@ module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
                 if(vld[block_num] == 0) begin
                     vld[block_num] = 1;
                     cache_data[block_num] = '0;
-                    tag_number[block_num] = address_in[15:6];
+                    tag_number[block_num] = address_in[15:5];
+                end
+
+                else if (vld[block_num == 1]) begin
+                    /* If data is already present in this cache line, and we are writing to it
+                       We should evict the current data first */
+                       send_to_mem(block_num);
                 end
 
                 if(address_in % 4 == 0) begin
@@ -193,15 +205,13 @@ module cache #(parameter CACHE_NUM_COL = 8 /*Number of rows in the cache */
             /* Let's write a cache block to memory */
             to_evict[block_num] = 1;
             
-            for(int i = 0; i < CACHE_NUM_COL; i++ ) begin
+            for(int i = 0; i < CACHE_NUM_ROW; i++ ) begin
                 if(to_evict[i] == 1) begin
                     send_to_mem(i);
                 end
             end
 
             to_evict[block_num] = 0;    
-
-
 
             read_success = 0;
             $display("Block number is %d", block_num);
@@ -217,13 +227,15 @@ module RAM
     )
    (
     input clk, 
-    input [WIDTH_AD] address_in,
-    input [D_WIDTH] data_in,
-    output reg [D_WIDTH] data_out,
+    input [WIDTH_AD-1:0] address_in,
+    input [D_WIDTH-1:0] data_in,
+    output reg [D_WIDTH-1:0] data_out,
     input wren,
     output reg load_completed,
     output reg store_completed,
-    input store_ack
+    input store_ack,
+    input mem_load_req,
+    output reg load_toggle
     );
 
 reg [D_WIDTH-1:0] mem [0:(2**WIDTH_AD)-1];
@@ -236,6 +248,12 @@ always @(posedge clk) begin
     else if(wren) begin 
         mem[address_in] <= data_in;
         store_completed <= 1;
+    end
+
+    if(mem_load_req) begin      /* If the cache requests a cache block sized data*/
+        load_completed <= 1;
+    end else begin
+        load_completed <= 0;
     end
     
     if(wren) begin
